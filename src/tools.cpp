8a00f0364f29f90fe7cbfe451abacb9e1795421c
zhangxiaogang <zhangxiaogang@xiaomi.com>
2017-07-04 14:59:49 +0800

    add rmse in tools

--- src/tools.cpp
+++ src/tools.cpp
@@ -12,7 +12,25 @@ Tools::~Tools() {}
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
   /**
-  TODO:
     * Calculate the RMSE here.
   */
+    VectorXd rmse(4);
+    rmse << 0, 0, 0, 0;
+
+    //check the inputs validation
+    if (estimations.size() <= 0 && estimations.size() != ground_truth.size()) {
+        cout << "(warning) the input for rmse calculation is incorrect " << endl;
+        return rmse;
+    }
+
+    for (int i=0; i < estimations.size(); ++i) {
+        VectorXd residual = estimations[i] - ground_truth[i];
+        residual = residual.array() * residual.array();
+        rmse += residual;
+    }
+
+    // calculate the suqare root
+    rmse = rmse/estimations.size();
+    rmse = rmse.array().sqrt();
+    return rmse;
 }
